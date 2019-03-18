9611bf6ea18774956b47e4578bbf9c8c05b5a8cc
Luke King <lkingfrm@gmail.com>
2019-03-18 16:09:31 -0600

    new database

---
+++ server/auth/session.js
@@ -0,0 +1,24 @@
+var expressSession = require("express-session");
+var mongoStore = require("connect-mongodb-session")(expressSession);
+
+var store = new mongoStore({
+  uri: "mongodb://bobsburgers:burgersnow*@den1.mongo1.gear.host:27001/bobsburgers", //CHANGE ME!!!!!!
+  collection: "Sessions"
+});
+
+store.on("error", function (err) {
+  console.log("[SESSION ERROR]", err);
+});
+
+// @ts-ignore
+var session = expressSession({
+  secret: "you should change this", //CHANGE ME!!!!
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
+  },
+  store,
+  resave: true,
+  saveUninitialized: true
+});
+
+module.exports = session;
---
+++ server/db/gearhost-config.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var connectionString = "mongodb://bobsburgers:burgersnow*@den1.mongo1.gear.host:27001/bobsburgers" //CHANGE ME!!!!!
+var connection = mongoose.connection
+
+
+mongoose.connect(connectionString, {
+  useMongoClient: true
+})
+
+connection.on('error', err => {
+  console.log('ERROR FROM DATABASE: ', err)
+})
+
+
+connection.once('open', () => {
+  console.log('Connected to Database')
+})

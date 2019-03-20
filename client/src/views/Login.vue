305151288445bd687bed76d012978923db300b36
Benjamin Dustin <Dustinbeenjamin@gmail.com>
2019-03-20 08:50:24 -0600

    meh

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,41 +1,5 @@
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             <template>
   <div class="login container-fluid">
-<<<<<<< HEAD
-    <div class="row">
-      <div class="col">
-        <form @submit.prevent="loginUser">
-          <input type="email" v-model="creds.email" placeholder="email">
-          <input type="password" v-model="creds.password" placeholder="password">
-          <button class="btn btn-sm bg-dark text-light" type="submit">Login</button>
-        </form>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <button
-          class="btn btn-sm bg-dark text-light"
-          @click="showAdminLogin = !showAdminLogin"
-        >Admin Login</button>
-        <div v-show="showAdminLogin">
-          <form v-if="loginForm" @submit.prevent="adminLogin">
-            <input type="email" v-model="adminCreds.email">
-            <input type="password" v-model="adminCreds.password">
-          </form>
-
-          <form v-else @submit.prevent="register">
-            <input type="text" v-model="newAdmin.name" placeholder="name">
-            <input type="text" v-model="newAdmin.email" placeholder="email">
-            <input type="text" v-model="newAdmin.password" placeholder="password">
-            <button type="submit">Create Account</button>
-          </form>
-          <div class="action" @click="loginForm = !loginForm">
-            <p v-if="loginForm">No account? Click here to Register</p>
-            <p v-else>Already have an account? Click here to Login</p>
-          </div>
-        </div>
-      </div>
-    </div>
-=======
 
     <!-- BACKDOOR TO ADD ACCOUNTS -->
     <!-- <form @submit.prevent="register">
@@ -45,7 +9,6 @@
       <button type="submit">Create Account</button>
     </form> -->
         
->>>>>>> e7373561d4c264db8cea452102a94eebb8671e0a
   </div>
 </template>
 

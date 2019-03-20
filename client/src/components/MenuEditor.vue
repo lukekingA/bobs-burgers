4c7096a9d98201753629c67637288b0d5f37957f
Luke King <lkingfrm@gmail.com>
2019-03-20 00:44:00 -0600

    admin.vue menuMaker.vue

---
+++ client/src/components/MenuEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="menu-editor">
+
+    <h1>Menu Editor</h1>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'menu-editor',
+    data() {
+      return {}
+    },
+    computed: {},
+    methods: {},
+    components: {}
+  }
+</script>
+
+
+<style scoped>
+
+</style>

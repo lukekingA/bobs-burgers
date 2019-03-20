0e967790f83091b1343372689325bbf63e71f433
Benjamin Dustin <Dustinbeenjamin@gmail.com>
2019-03-20 11:35:26 -0600

    changes

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,40 +1,41 @@
 <template>
-  <div class="login container-fluid">
-    <div class="row">
-      <div class="col">
-        <form @submit.prevent="loginUser">
-          <input type="email" v-model="creds.email" placeholder="email">
-          <input type="password" v-model="creds.password" placeholder="password">
-          <button class="btn btn-sm bg-dark text-light" type="submit">Login</button>
-        </form>
+    <h1>LOGIN VUE</h1>
+    <!-- <div class="login container-fluid">
+      <div class="row">
+        <div class="col">
+          <form @submit.prevent="loginUser">
+            <input type="email" v-model="creds.email" placeholder="email">
+            <input type="password" v-model="creds.password" placeholder="password">
+            <button class="btn btn-sm bg-dark text-light" type="submit">Login</button>
+          </form>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <button
-          class="btn btn-sm bg-dark text-light"
-          @click="showAdminLogin = !showAdminLogin"
-        >Admin Login</button>
-        <div v-show="showAdminLogin">
-          <form v-if="loginForm" @submit.prevent="adminLogin">
-            <input type="email" v-model="adminCreds.email">
-            <input type="password" v-model="adminCreds.password">
-          </form>
+      <div class="row">
+        <div class="col">
+          <button
+            class="btn btn-sm bg-dark text-light"
+            @click="showAdminLogin = !showAdminLogin"
+          >Admin Login</button>
+          <div v-show="showAdminLogin">
+            <form v-if="loginForm" @submit.prevent="adminLogin">
+              <input type="email" v-model="adminCreds.email">
+              <input type="password" v-model="adminCreds.password">
+            </form>
 
-          <form v-else @submit.prevent="register">
-            <input type="text" v-model="newAdmin.name" placeholder="name">
-            <input type="text" v-model="newAdmin.email" placeholder="email">
-            <input type="text" v-model="newAdmin.password" placeholder="password">
-            <button type="submit">Create Account</button>
-          </form>
-          <div class="action" @click="loginForm = !loginForm">
-            <p v-if="loginForm">No account? Click here to Register</p>
-            <p v-else>Already have an account? Click here to Login</p>
+            <form v-else @submit.prevent="register">
+              <input type="text" v-model="newAdmin.name" placeholder="name">
+              <input type="text" v-model="newAdmin.email" placeholder="email">
+              <input type="text" v-model="newAdmin.password" placeholder="password">
+              <button type="submit">Create Account</button>
+            </form>
+            <div class="action" @click="loginForm = !loginForm">
+              <p v-if="loginForm">No account? Click here to Register</p>
+              <p v-else>Already have an account? Click here to Login</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
+    </div> -->
 </template>
 
 <script>

07054706995389d2d9d716b9620df4491e31c3e2
Andrea <avertrees@yahoo.com>
2019-03-21 17:10:10 -0600

    commit-buttons

--- client/src/views/Order.vue
+++ client/src/views/Order.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="Order">
     <div class="row">
-      <div class="col-3 d-flex flex-column align-items-start">
-        <button class="btn btn-lg rounded drop-shadow">COMBOS</button>
-        <button class="btn btn-lg rounded drop-shadow">BURGERS</button>
-        <button class="btn btn-lg rounded drop-shadow">CHICKEN</button>
-        <button class="btn btn-lg rounded drop-shadow">DRINKS</button>
-        <button class="btn btn-lg rounded drop-shadow">SIDES</button>
+      <div class="col-3 d-flex mt-2 flex-column align-items-start">
+        <button
+          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
+        >CHICKEN</button>
+        <button
+          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
+        >DRINKS</button>
+        <button
+          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
+        >COMBOS</button>
+        <button
+          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
+        >SIDES</button>
+        <button
+          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
+        >BURGERS</button>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Order",
-    props: [],
-    data() {
-      return {};
-    },
-    computed: {},
-    methods: {},
-    components: {}
-  };
+export default {
+  name: "Order",
+  props: [],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
 </script>
 
 <style>
-
+.button-width {
+  width: 200px;
+}
 </style>

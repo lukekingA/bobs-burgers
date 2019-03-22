d40b709caa7db3a6b7af26e7cdaa1e681704feb1
Luke King <lkingfrm@gmail.com>
2019-03-21 22:15:13 -0600

    reorder buttons

--- client/src/views/Order.vue
+++ client/src/views/Order.vue
@@ -2,41 +2,31 @@
   <div class="Order">
     <div class="row">
       <div class="col-3 d-flex mt-2 flex-column align-items-start">
-        <button
-          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >CHICKEN</button>
-        <button
-          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >DRINKS</button>
-        <button
-          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >COMBOS</button>
-        <button
-          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >SIDES</button>
-        <button
-          class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >BURGERS</button>
+        <button class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow">COMOBS</button>
+        <button class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow">BURGERS</button>
+        <button class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow">CHICKEN</button>
+        <button class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow">SIDES</button>
+        <button class="btn button-width mb-2 btn-lg border-light bg-dark text-light rounded drop-shadow">DRINKS</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Order",
-  props: [],
-  data() {
-    return {};
-  },
-  computed: {},
-  methods: {},
-  components: {}
-};
+  export default {
+    name: "Order",
+    props: [],
+    data() {
+      return {};
+    },
+    computed: {},
+    methods: {},
+    components: {}
+  };
 </script>
 
 <style>
-.button-width {
-  width: 200px;
-}
+  .button-width {
+    width: 200px;
+  }
 </style>

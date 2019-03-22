2737744141aa1f5e11995ec05bca7cc6ee4871be
Luke King <lkingfrm@gmail.com>
2019-03-22 14:21:47 -0600

    restructure

---
+++ client/src/components/Dropdowns/DropdownSpecials.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dropdown-specials">
+
+    DROPDOWN SPECIALS
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'dropdown-specials',
+    data() {
+      return {}
+    },
+    computed: {},
+    methods: {},
+    components: {}
+  }
+</script>
+
+
+<style scoped>
+
+</style>
--- client/src/views/Order.vue
+++ client/src/views/Order.vue
@@ -2,81 +2,69 @@
   <div class="Order">
     <div class="row">
       <div class="col-3 d-flex mt-3 flex-column align-items-start">
-        <button
-          @click="switchView('showCombo')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >COMOBS</button>
-        <button
-          @click="switchView('showBurgers')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >BURGERS</button>
-        <button
-          @click="switchView('showChicken')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >CHICKEN</button>
-        <button
-          @click="switchView('showSides')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >SIDES</button>
-        <button
-          @click="switchView('showDrinks')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >DRINKS</button>
+
+        <button @click="switchView('sandwichDropdown')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">Meal</button>
+
+        <button @click="switchView('sideDropdown')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">Sides</button>
+        <button @click="switchView('drinkDropdown')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">Drink</button>
+        <button @click="switchView('commentDropdown')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">Comments</button>
+        <button @click="switchView('specialDropdown')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">Specials</button>
       </div>
-      <combo-drop-down v-if="showCombo"></combo-drop-down>
-      <burger-drop-down v-if="showBurgers"></burger-drop-down>
-      <chicken-drop-down v-if="showChicken"></chicken-drop-down>
-      <side-drop-down v-if="showSides"></side-drop-down>
-      <drink-drop-down v-if="showDrinks"></drink-drop-down>
+
+      <dropdown-sandwiches v-show="sandwichDropdown"></dropdown-sandwiches>
+      <dropdown-sides v-show="sideDropdown"></dropdown-sides>
+      <dropdown-drinks v-show="drinkDropdown"></dropdown-drinks>
+      <dropdown-comments v-show="commentDropdown"></dropdown-comments>
+      <dropdown-specials v-show="specialDropdown"></dropdown-specials>
     </div>
   </div>
 </template>
 
 <script>
-import ComboDropDown from "@/components/ComboDropdown.vue";
-import BurgerDropDown from "@/components/BurgerDropdown.vue";
-import ChickenDropDown from "@/components/ChickenDropdown.vue";
-import SideDropDown from "@/components/SideDropdown.vue";
-import DrinkDropDown from "@/components/DrinkDropdown.vue";
-export default {
-  name: "Order",
-  props: [],
-  data() {
-    return {
-      showCombo: true,
-      showBurgers: false,
-      showChicken: false,
-      showSides: false,
-      showDrinks: false
-    };
-  },
-  computed: {},
-  methods: {
-    switchView(selection) {
-      let shows = [
-        "showCombo",
-        "showBurgers",
-        "showChicken",
-        "showSides",
-        "showDrinks"
-      ];
-      let newthis = this;
-      shows.forEach(j => (newthis[j] = false));
-      this[selection] = true;
+  import DropdownSandwiches from "@/components/Dropdowns/DropdownSandwiches.vue";
+  import DropdownDrinks from "@/components/Dropdowns/DropdownDrinks.vue";
+  import DropdownSides from "@/components/Dropdowns/DropdownSides.vue";
+  import DropdownComments from "@/components/Dropdowns/DropdownComments.vue";
+  import DropdownSpecials from "@/components/Dropdowns/DropdownSpecials.vue"
+  export default {
+    name: "Order",
+    props: [],
+    data() {
+      return {
+        sandwichDropdown: true,
+        sideDropdown: false,
+        drinkDropdown: false,
+        commentDropdown: false,
+        specialDropdown: false
+      };
+    },
+    computed: {},
+    methods: {
+      switchView(selection) {
+        let shows = [
+          "sandwichDropdown",
+          "sideDropdown",
+          "drinkDropdown",
+          "commentDropdown",
+          "specialDropdown"
+        ];
+        let newthis = this;
+        shows.forEach(j => (newthis[j] = false));
+        this[selection] = true;
+      }
+    },
+    components: {
+      DropdownSandwiches,
+      DropdownSides,
+      DropdownDrinks,
+      DropdownComments,
+      DropdownSpecials
     }
-  },
-  components: {
-    ComboDropDown,
-    BurgerDropDown,
-    ChickenDropDown,
-    SideDropDown,
-    DrinkDropDown
-  }
-};
+  };
 </script>
 
 <style>
-.button-width {
-  width: 200px;
-}
+  .button-width {
+    width: 200px;
+  }
 </style>

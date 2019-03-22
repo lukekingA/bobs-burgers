5992f15de56f130bd8f58277d02a3849a8fb6dca
Luke King <lkingfrm@gmail.com>
2019-03-22 10:33:44 -0600

    fix

--- client/src/views/Order.vue
+++ client/src/views/Order.vue
@@ -2,26 +2,11 @@
   <div class="Order">
     <div class="row">
       <div class="col-3 d-flex mt-3 flex-column align-items-start">
-        <button
-          @click="switchView('showCombo')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >COMOBS</button>
-        <button
-          @click="switchView('showBurger')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >BURGERS</button>
-        <button
-          @click="switchView('showChicken')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >CHICKEN</button>
-        <button
-          @click="switchView('showSide')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >SIDES</button>
-        <button
-          @click="switchView('showDrink')"
-          class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow"
-        >DRINKS</button>
+        <button @click="switchView('showCombo')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">COMOBS</button>
+        <button @click="switchView('showBurgers')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">BURGERS</button>
+        <button @click="switchView('showChicken')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">CHICKEN</button>
+        <button @click="switchView('showSides')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">SIDES</button>
+        <button @click="switchView('showDrinks')" class="btn button-width mb-3 btn-lg border-light bg-dark text-light rounded drop-shadow">DRINKS</button>
       </div>
       <combo-drop-down v-if="showCombo"></combo-drop-down>
       <burger-drop-down v-if="showBurgers"></burger-drop-down>
@@ -33,50 +18,50 @@
 </template>
 
 <script>
-import ComboDropDown from "@/components/ComboDropdown.vue";
-import BurgerDropDown from "@/components/BurgerDropdown.vue";
-import ChickenDropDown from "@/components/ChickenDropdown.vue";
-import SideDropDown from "@/components/SideDropdown.vue";
-import DrinkDropDown from "@/components/DrinkDropdown.vue";
-export default {
-  name: "Order",
-  props: [],
-  data() {
-    return {
-      showCombo: true,
-      showBurgers: false,
-      showChicken: false,
-      showSides: false,
-      showDrinks: false
-    };
-  },
-  computed: {},
-  methods: {
-    switchView(selection) {
-      let shows = [
-        "showCombo",
-        "showBurgers",
-        "showChicken",
-        "showSides",
-        "showDrinks"
-      ];
-      let newthis = this;
-      shows.forEach(j => (newthis[j] = false));
-      this[selection] = true;
+  import ComboDropDown from "@/components/ComboDropdown.vue";
+  import BurgerDropDown from "@/components/BurgerDropdown.vue";
+  import ChickenDropDown from "@/components/ChickenDropdown.vue";
+  import SideDropDown from "@/components/SideDropdown.vue";
+  import DrinkDropDown from "@/components/DrinkDropdown.vue";
+  export default {
+    name: "Order",
+    props: [],
+    data() {
+      return {
+        showCombo: true,
+        showBurgers: false,
+        showChicken: false,
+        showSides: false,
+        showDrinks: false
+      };
+    },
+    computed: {},
+    methods: {
+      switchView(selection) {
+        let shows = [
+          "showCombo",
+          "showBurgers",
+          "showChicken",
+          "showSides",
+          "showDrinks"
+        ];
+        let newthis = this;
+        shows.forEach(j => (newthis[j] = false));
+        this[selection] = true;
+      }
+    },
+    components: {
+      ComboDropDown,
+      BurgerDropDown,
+      ChickenDropDown,
+      SideDropDown,
+      DrinkDropDown
     }
-  },
-  components: {
-    ComboDropDown,
-    BurgerDropDown,
-    ChickenDropDown,
-    SideDropDown,
-    DrinkDropDown
-  }
-};
+  };
 </script>
 
 <style>
-.button-width {
-  width: 200px;
-}
+  .button-width {
+    width: 200px;
+  }
 </style>

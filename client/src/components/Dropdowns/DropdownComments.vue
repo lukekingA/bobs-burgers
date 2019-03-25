091df55af7ba94c5a7a210dc0a4678f0748e7170
Michael Graves <msgraves99@gmail.com>
2019-03-25 17:26:11 -0600

    comments

--- client/src/components/Dropdowns/DropdownComments.vue
+++ client/src/components/Dropdowns/DropdownComments.vue
@@ -2,21 +2,11 @@
   <div class="dropdownComments">
     <div>
       <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle mb-2"
-          type="button"
-          id="dropdownMenu1"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >Comments</button>
+        <button class="btn btn-secondary dropdown-toggle mb-2" type="button" id="dropdownMenu1" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">Comments</button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
-          <span
-            @click="commentSelect"
-            class="dropdown-item border-bottom"
-            v-for="comment in comments"
-            :key="comment._id"
-          >{{comment.comment}}</span>
+          <span @click="commentSelection.push(comment)" class="dropdown-item border-bottom" v-for="comment in comments"
+            :key="comment._id">{{comment.comment}}</span>
           <span>
             <input type="text" class="justify-content-center">
           </span>
@@ -29,27 +19,23 @@
 
 
 <script>
-export default {
-  name: "dropdownComments",
-  props: [],
-  data() {
-    return {
-      commentSelection: []
-    };
-  },
-  mounted() {
-    this.$store.dispatch("getComments");
-  },
-  computed: {
-    comments() {
-      return this.$store.state.comments;
-    }
-  },
-  methods: {
-    commentSelect() {
-      commentSelection.push(comment);
-    }
-  },
-  components: {}
-};
+  export default {
+    name: "dropdownComments",
+    props: [],
+    data() {
+      return {
+        commentSelection: []
+      };
+    },
+    mounted() {
+      this.$store.dispatch("getComments");
+    },
+    computed: {
+      comments() {
+        return this.$store.state.comments;
+      }
+    },
+    methods: {},
+    components: {}
+  };
 </script>
